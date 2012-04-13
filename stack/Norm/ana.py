44e84ce8e4e1844b2eb24cc26106c19d504bca6c
Paul Yang <paulyang1990@gmail.com>
2012-04-13 22:05:11 +0800

    modified the summary generation function

--- stack/Norm/ana.py
+++ stack/Norm/ana.py
@@ -1,12 +1,108 @@
 import os
 import json
 import sys
-sys.path.append('~/Documents/FYT/fyp/')
+mypysourcepath = '/home/paul/Documents/FYT/fyp/'
+sys.path.append(mypysourcepath)
 from pysource import api
 
-questionpath = "../data/questions/"
-answerpath = "../data/answers/"
-def extractCorpus(start,end):
+questionpath = "data/questions/"
+answerpath = "data/answers/"
+
+def parseSource_File(filename):
+    with open(filename) as f: 
+        node = api.parse_source(f.read())
+    import_names = list(api.get_import_names(node))
+    package = []
+    funcs = []
+    for item in import_names:
+        if len(item.split('.')) >1:
+            pac = item.split('.')[0]
+            fun = item.split('.')[1]
+            package.append(pac)
+            funcs.append(fun)
+        else:
+            package.append(item)
+    return set(package),set(funcs)
+
+def parseSource_Code(string):
+    node = api.parse_source(string)
+    import_names = list(api.get_import_names(node))
+    package = []
+    funcs = []
+    for item in import_names:
+        if len(item.split('.')) >1:
+            pac = item.split('.')[0]
+            fun = item.split('.')[1]
+            package.append(pac)
+            funcs.append(fun)
+        else:
+            package.append(item)
+    return list(set(package)),list(set(funcs))
+
+def extractGraph_corpus(start,end):
+    qFileList = sorted(os.listdir(questionpath))
+    aFileList = sorted(os.listdir(answerpath))
+    for i,item in enumerate(qFileList):
+        qFileList[i] = questionpath+qFileList[i]
+    for i,item in enumerate(aFileList):
+        aFileList[i] = answerpath+aFileList[i]
+
+    senList = []
+    for i,fileName in enumerate(qFileList[start:end]):
+        f = open(fileName,'r')
+        a = json.load(f)
+        for item in a:
+            if type(item) == list:
+                for myDict in item:
+                    if type(myDict) == dict:
+                        if myDict.has_key('code'):
+                            if myDict['code']!= []:
+                                senList.append(myDict['code'])
+                    elif type(myDict) == list:
+                        for subDict in myDict:
+                            if subDict.has_key('code'):
+                                if subDict['code']!= []:
+                                    senList.append(subDict['code'])
+            elif type(item) == dict:
+                if item.has_key('code'):
+                    if item['code']!= []:
+                        senList.append(item['code'])
+
+    for i,fileName in enumerate(aFileList[start:end]):
+        f = open(fileName,'r')
+        a = json.load(f)
+        for item in a:
+            if type(item) == list:
+                for myDict in item:
+                    if type(myDict) == dict:
+                        if myDict.has_key('code'):
+                            if myDict['code']!= []:
+                                senList.append(myDict['code'])
+                    elif type(myDict) == list:
+                        for subDict in myDict:
+                            if subDict.has_key('code'):
+                                if subDict['code']!= []:
+                                    senList.append(subDict['code'])
+            elif type(item) == dict:
+                if item.has_key('code'):
+                    if item['code']!= []:
+                        senList.append(item['code'])
+    pack_count = defaultdict(int)
+    for item in senList:
+        item = nltk.clean_html(item)
+        try:
+            packs,funcs = parseSource_Code(item.decode("utf8"))
+            for pack in packs:
+                pack_count[pack] +=1
+        except SyntaxError:
+            pass
+        except UnicodeEncodeError:
+            print item 
+    return pack_count
+
+
+
+def extractTFIDF_corpus(start,end):
     qFileList = sorted(os.listdir(questionpath))
     aFileList = sorted(os.listdir(answerpath))
     for i,item in enumerate(qFileList):
@@ -103,42 +199,12 @@ def getTFIDF(senList,docList):
 
     return tfidf
 
-def parseFile(filename):
-    with open(filename) as f: 
-        node = api.parse_source(f.read())
-    import_names = list(api.get_import_names(node))
-    package = []
-    funcs = []
-    for item in import_names:
-        if len(item.split('.')) >1:
-            pac = item.split('.')[0]
-            fun = item.split('.')[1]
-            package.append(pac)
-            funcs.append(fun)
-        else:
-            package.append(item)
-    return set(package),set(funcs)
-
-def parseCode(string):
-    node = api.parse_source(string)
-    import_names = list(api.get_import_names(node))
-    package = []
-    funcs = []
-    for item in import_names:
-        if len(item.split('.')) >1:
-            pac = item.split('.')[0]
-            fun = item.split('.')[1]
-            package.append(pac)
-            funcs.append(fun)
-        else:
-            package.append(item)
-    return set(package),set(funcs)
 
 
 if __name__=="__main__":
     import nltk
     import math
     from collections import defaultdict
-    deb = None
-    sen,doc = extractCorpus(0,99999)
-    tfidf = getTFIDF(sen,doc)
+    #deb = None
+    #sen,doc = extractTFIDF_corpus(0,99999)
+    #tfidf = getTFIDF(sen,doc)
